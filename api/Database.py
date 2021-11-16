a095ad4151f50bc01d984aaa055b0ec43d08c41b
Shahad Althobaiti <shahad.althobaiti@gmail.com>
2021-11-16 16:28:05 -0500

    added a query

--- api/Database.py
+++ api/Database.py
@@ -474,8 +474,12 @@ class Database:
         '''
         args = (semester, year)
         query = """DELETE FROM Takes WHERE semester=\'%s\' AND year=%s;""" % args
+        query 1= """DELETE FROM academic_semesterWHERE semester=\'%s\' AND year=%s;""" % args
+
         conn = self.connect()
         cursor = conn.execute(query)
+        cursor = conn.execute(query2)
+
         self.close(conn)
 
     def add_schedule_from_file(self):
